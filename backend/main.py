17acaac5ebc74670b53937f035b08c13781146f9
Yanis Bouchelaghem <bouchelaghemyanis7@gmail.com>
2024-12-01 17:06:51 +0100

    Implemented basic color highlighting, but it's not dynamic

--- backend/main.py
+++ backend/main.py
@@ -3,15 +3,27 @@ import uvicorn
 from contextlib import asynccontextmanager
 from gliner import GLiNER
 from models.inference_input import InferenceInput
-
+from fastapi.middleware.cors import CORSMiddleware
 
 print("------------------ Initializing model... ------------------")
 # Initialize GLiNER with the base model
 model = GLiNER.from_pretrained("urchade/gliner_medium-v2.1")
 print("------------------ Model initialized. ------------------")
 
+origins = [
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    "*"
+]
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PATCH", "PUT", "UPDATE", "HEAD", "OPTIONS"],
+    allow_headers=["Access-Control-Allow-Headers", 'Content-Type', 'Authorization', 'Access-Control-Allow-Origin']
+)
 
 @app.get("/")
 async def root():
